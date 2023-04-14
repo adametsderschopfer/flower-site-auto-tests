6ee09d0ffc0406b2b2c694ef358b914351e19d09
Adamets $ Vladislav <v.adamets@itspecial.net>
2023-04-14 22:55:01 +0200

    changes

--- src/test/java/CalculatorTestCases.java
+++ src/test/java/CalculatorTestCases.java
@@ -4,17 +4,54 @@ import org.cvetok39.calculator.core.CalculatorParams;
 import org.cvetok39.calculator.tests.CalculatorAddFlowerAndCompareInCartTest;
 
 public class CalculatorTestCases {
-    public static void main(String[] args) {
+    public static void testWithParams(
+            String flower,
+            int height,
+            int bouquetsCount,
+            int flowersCount,
+            CalculatorPack pack,
+            CalculatorColor color
+    ) {
         CalculatorParams params = new CalculatorParams();
 
-        params.setFlower("Роза");
-        params.setHeight(50);
-        params.setBouquetsCount(2);
-        params.setPack(CalculatorPack.FILM);
-        params.setColor(CalculatorColor.RED);
-        params.setFlowersCount(12);
+        params.setFlower(flower);
+        params.setHeight(height);
+        params.setBouquetsCount(bouquetsCount);
+        params.setPack(pack);
+        params.setColor(color);
+        params.setFlowersCount(flowersCount);
 
-        CalculatorAddFlowerAndCompareInCartTest calculatorTest = new CalculatorAddFlowerAndCompareInCartTest(params);
+        CalculatorAddFlowerAndCompareInCartTest calculatorTest
+                = new CalculatorAddFlowerAndCompareInCartTest(params);
         calculatorTest.run();
+    }
+
+    public static void main(String[] args) {
+        testWithParams(
+                "Роза",
+                50,
+                2,
+                12,
+                CalculatorPack.FILM,
+                CalculatorColor.RED
+        );
+
+        testWithParams(
+                "Роза",
+                50,
+                20,
+                12,
+                CalculatorPack.FILM,
+                CalculatorColor.RED
+        );
+
+        testWithParams(
+                "Роза",
+                50,
+                2,
+                12,
+                CalculatorPack.FILM,
+                CalculatorColor.RED
+        );
     }
 }
